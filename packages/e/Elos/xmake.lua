65d5adb2526d4ca3d7427dd97b51f5026a2c3c9c
Arnav Mehta <arnavdatt@gmail.com>
2025-02-16 21:54:56 +0000

    Udpate hash

--- packages/e/Elos/xmake.lua
+++ packages/e/Elos/xmake.lua
@@ -1,7 +1,7 @@
 package("Elos")
 	set_description("A colletion C++ helper classes")
 	add_urls("https://github.com/ArnavMehta3000/Elos.git")
-	add_versions("1.0.0", "58b8065eba0fa86283d26cb73592d471b99cf32a")
+	add_versions("1.0.0", "df42f8171d1536d5d68ab4bcc0c2404996d6d334")
 	set_license("MIT")
 	set_kind("library")
 
@@ -11,7 +11,7 @@ package("Elos")
     on_install("windows|x64", function (package)
 		package:add("syslinks", "user32", "gdi32", "dwmapi", "shcore", "Comctl32")
 		package:add("defines", "UNICODE", "_UNICODE", "NOMINMAX", "NOMCX", "NOSERVICE", "NOHELP", "WIN32_LEAN_AND_MEAN")
-		
+
     	local configs = {}
 
 		if package:config("shared") then
@@ -27,7 +27,7 @@ package("Elos")
             configs.runtimes = "MD"
 			package:add("defines", "ELOS_BUILD_DEBUG=0")
         end
-		
+
 		if package:config("shared") then
 			package:add("defines", "ELOS_EXPORTS")
 		end
@@ -39,7 +39,7 @@ package("Elos")
 
 	on_test(function (package)
         assert(package:check_cxxsnippets({ test = [[
-            void test() 
+            void Test()
 			{
                 Elos::i32 = -1;
                 Elos::u32 = 0;
