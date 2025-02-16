c72c720bb5aadd5c977fd5fc1d222d1f71aa967b
Arnav Mehta <arnavdatt@gmail.com>
2025-02-16 03:13:12 +0000

    Update urls and package links

--- packages/e/Elos/xmake.lua
+++ packages/e/Elos/xmake.lua
@@ -1,10 +1,11 @@
 package("Elos")
 	set_description("A colletion C++ helper classes")
-	add_urls("https://github.com/ArnavMehta3000/Elos")
+	add_urls("https://github.com/ArnavMehta3000/Elos.git")
 	add_versions("1.0.0", "5cf880513f12efaa28b7109ab96188da229b8274")
 	set_license("MIT")
 
-    add_configs("shared", { description = "Build as shared library", default = false, type = "boolean" })
+    add_configs("debug", { builtin = true, description = "Enable debug symbols.", default = false, type = "boolean", readonly = true })
+	add_configs("shared", { description = "Build shared library.", default = true, type = "boolean", readonly = true })
 
     on_install("windows|x64", function (package)
     	if package:is_debug() then
@@ -13,8 +14,9 @@ package("Elos")
     		package:add_defines("ELOS_BUILD_DEBUG=0")
     	end
 
+		package:add("links", "user32", "gdi32", "dwmapi", "shcore", "Comctl32")
+		
     	local configs = {}
-
     	table.insert(configs, "--BuildShared=" .. (package:config("shared") and "y" or "n"))
 
     	import("package.tools.xmake").install(package, configs)
