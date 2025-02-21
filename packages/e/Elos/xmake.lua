5768a10773a1de25cb5dc02ceb38f54eff56a240
Arnav Mehta <arnavdatt@gmail.com>
2025-02-21 22:38:02 +0000

    Updat Elos package commit hash

--- packages/e/Elos/xmake.lua
+++ packages/e/Elos/xmake.lua
@@ -1,7 +1,7 @@
 package("Elos")
 	set_description("A colletion C++ helper classes")
 	add_urls("https://github.com/ArnavMehta3000/Elos.git")
-	add_versions("1.0.0", "3e7a23096d9bbfa5dd530bc06e9ee29f87d910e5")
+	add_versions("1.0.0", "fc95dba49f9feddff84b8b39b0f9b619c4bf7efe")
 	set_license("MIT")
 	set_kind("library")
 
