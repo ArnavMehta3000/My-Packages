89d9604dca7e35f9fe6258350256cae5b87f73aa
Arnav Mehta <arnavdatt@gmail.com>
2025-02-27 13:02:20 +0000

    Update Elos

--- packages/e/Elos/xmake.lua
+++ packages/e/Elos/xmake.lua
@@ -1,43 +1,43 @@
 package("Elos")
 	set_description("A colletion C++ helper classes")
 	add_urls("https://github.com/ArnavMehta3000/Elos.git")
-	add_versions("1.0.0", "fc95dba49f9feddff84b8b39b0f9b619c4bf7efe")
+	add_versions("1.0.0", "4033b0b2a961262fd953faf2424c67411c66886b")
 	set_license("MIT")
 	set_kind("library")
 
-    on_install("windows|x64", function (package)
+	on_install("windows|x64", function (package)
 		package:add("syslinks", "user32", "gdi32", "dwmapi", "shcore", "Comctl32")
 		package:add("defines", "UNICODE", "_UNICODE", "NOMINMAX", "NOMCX", "NOSERVICE", "NOHELP", "WIN32_LEAN_AND_MEAN")
 
-    	local configs = {}
+		local configs = {}
 
 		if package:config("shared") then
-            configs.BuildShared = true
-        end
+			configs.BuildShared = true
+		end
 
 		if package:config("debug") then
-            configs.mode = "debug"
+			configs.mode = "debug"
 			package:add("defines", "ELOS_BUILD_DEBUG=1")
-        else
-            configs.mode = "release"
+		else
+			configs.mode = "release"
 			package:add("defines", "ELOS_BUILD_DEBUG=0")
-        end
+		end
 
 		if package:config("shared") then
 			package:add("defines", "ELOS_EXPORTS")
 		end
 
-    	import("package.tools.xmake").install(package, configs, { target = "Elos" })
-    end)
+		import("package.tools.xmake").install(package, configs, { target = "Elos" })
+	end)
 
 	on_test(function (package)
-        assert(package:check_cxxsnippets({ test = [[
-            void Test()
+		assert(package:check_cxxsnippets({ test = [[
+			void Test()
 			{
-                Elos::i32 i = -1;
-                Elos::u32 u = 0;
-            }
-        ]] }, { configs = {languages = "c++23"}, includes = "Elos/Common/StandardTypes.h"}))
-    end)
+				Elos::i32 i = -1;
+				Elos::u32 u = 0;
+			}
+		]] }, { configs = {languages = "c++23"}, includes = "Elos/Common/StandardTypes.h"}))
+	end)
 
 package_end()
